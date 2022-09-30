1524311b15a0d66c2ac84faff9b21127763dee2e
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-09-30 16:29:15 +0530

    Initial Commit

---
+++ Server/src/main/java/com/oopsie/shoppingapp/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.oopsie.shoppingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping(value = "/hello")
+	public String hello(){
+		return "Hello World";
+	}
+}
