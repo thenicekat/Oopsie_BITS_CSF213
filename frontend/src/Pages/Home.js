73c40d1abd01466eb83ab54b96766004df16da25
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-10-01 22:10:05 +0530

    Remove Commits

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -6,10 +6,6 @@ export default function Home() {
     <div className="justify-center items-center text-center flex flex-col h-screen align-middle bg-gray-300">
         <h1 className="text-5xl p-4">OOPSIE!</h1>
         <h3 className="text-3xl">Your One Stop Shopping Spot</h3>
-        {/* <div className="flex">
-            <Link className="p-3 bg-gray-400 hover:bg-blue-700 hover:text-white m-3 rounded-lg" to={'/register'}>Register</Link>
-            <Link className="p-3 bg-gray-400 hover:bg-blue-700 hover:text-white m-3 rounded-lg" to={'/login'}>Login</Link>
-        </div> */}
     </div>
     </>
   )
