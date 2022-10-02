36ecc5caa15bef66289890fa6fe1abd0013cff7b
Saphereye <adarshdas950@gmail.com>
2022-10-02 12:17:57 +0530

    Made all Product.js image+text into non clikables

--- frontend/src/Components/Product.js
+++ frontend/src/Components/Product.js
@@ -7,13 +7,13 @@ export default function Product({ product }) {
 
     return (
         <div className="max-w-sm rounded-lg border shadow-md bg-gray-800 border-gray-700 p-5 m-4">
-            <a href="#">
+            <div href="#">
                 <img className="rounded-t-lg" src={product.image} alt="" />
-            </a>
+            </div>
             <div className="p-5">
-                <a href="#">
+                <div href="#">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{product.name}</h5>
-                </a>
+                </div>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">${product.price}</p>
                 <button className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={() => dispatch(addToCart(product))}>
                     Add to Cart
