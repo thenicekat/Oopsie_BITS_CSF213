02132596cd9d2c50e3cdae9d3fb9656969c339e3
Saphereye <adarshdas950@gmail.com>
2022-10-03 17:57:36 +0530

    Optimised data for better understanding

--- frontend/src/data/data.js
+++ frontend/src/data/data.js
@@ -10,13 +10,13 @@ const data = {
             id: '2',
             name: 'Old Car',
             price: 1400,
-            image: 'https://picsum.photos/id/180/2400/1600'
+            image: 'https://picsum.photos/id/111/2400/1600'
         },
         {
             id: '3',
             name: 'W Shoes',
             price: 1400,
-            image: 'https://picsum.photos/id/180/2400/1600'
+            image: 'https://picsum.photos/id/21/2400/1600'
         },
     ],
 }
