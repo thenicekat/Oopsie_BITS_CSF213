ec838d5a9a8d3c1e75788a47f67b83f4b722e33d
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-10-11 18:35:53 +0530

    Create an inventory page

--- frontend/src/Pages/Inventory.js
+++ frontend/src/Pages/Inventory.js
@@ -1,5 +1,18 @@
+import { useSelector } from 'react-redux';
+
 export default function Inventory() {
-  return (
-    <div>Inventory</div>
-  )
+    const isAdmin = useSelector(state => state.auth.isAdmin);
+
+    return (
+        isAdmin ? (<div className='py-20 min-h-screen'>
+            <h2 className='text-3xl'>Inventory</h2>
+            <div className='flex flex-wrap justify-center items-center text-center align-middle'>
+                Add and Update Inventory
+            </div>
+        </div>) : (
+            <div className="justify-center items-center text-center flex flex-col h-screen align-middle">
+                <h3 className="text-3xl">You need to be an admin to access this page</h3>
+            </div>
+        )
+    )
 }
