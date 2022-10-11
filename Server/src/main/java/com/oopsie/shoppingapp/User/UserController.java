c9a6f230f43167fd54c05c6aec42dfd49070cf3e
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-10-12 00:30:12 +0530

    Remove Unnecessary Imports

--- Server/src/main/java/com/oopsie/shoppingapp/User/UserController.java
+++ Server/src/main/java/com/oopsie/shoppingapp/User/UserController.java
@@ -1,7 +1,6 @@
 package com.oopsie.shoppingapp.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
