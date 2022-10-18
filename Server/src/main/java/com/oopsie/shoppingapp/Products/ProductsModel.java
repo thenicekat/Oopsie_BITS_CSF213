66fcd371968ba301cfb8cc19054a77110778a9e6
Shivam <shivamtrivedia@gmail.com>
2022-10-18 19:36:26 +0530

    Product Class

---
+++ Server/src/main/java/com/oopsie/shoppingapp/Products/ProductsModel.java
@@ -0,0 +1,53 @@
+package com.oopsie.shoppingapp.Products;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class ProductsModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long id;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "price")
+    private double price;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQty() {
+        return quantity;
+    }
+
+    public void setQty(int quantity) {
+        this.quantity = quantity;
+    }
+    int a;
+}
+
+
