c7767ae0d0eb1238cd1d8e449ecb7980f95c9c1a
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-11-20 19:20:22 +0530

    auth reset

--- frontend/src/Context/authSlice.js
+++ frontend/src/Context/authSlice.js
@@ -1,10 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialUserState = {
-    isLoggedIn: true,
-    isManager: true,
+    isLoggedIn: false,
+    isManager: false,
     isAdmin: false,
-    isApproved: true
+    isApproved: false
 }
 
 export const authSlice = createSlice({
