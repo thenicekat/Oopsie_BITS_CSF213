d6ffbb139b4b08a899e26a8759a1e7cb897ad15b
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-12-02 23:59:23 +0530

    idk whatever this is

--- Server/src/main/resources/data.sql
+++ Server/src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO products VALUES(1,'MacBook',96900,12,4.6,'https://picsum.photos/id/180/2400/1600',''),
-(2,'Kindle',30000,,14,4,'https://picsum.photos/id/367/2400/1600',''),
-(3.'Camera',15000,12,4.2,'https://picsum.photos/id/250/2400/1600',''),
-(4,'Clock',600,13,4.2,'https://picsum.photos/id/175/2400/1600',''),
-(5,'Joystick',3500,13,3.9,'https://picsum.photos/id/96/2400/1600',''),
-(6,'Rolls Royce',2500000,13,4.8,'https://picsum.photos/id/111/2400/1600','');
+INSERT INTO `products` (`product_id`, `category`, `details`, `image`, `price`, `product_name`, `quantity`, `rating`) VALUES (NULL, 'electronics', 'lite bro', 'https://picsum.photos/id/111/2400/1600', '100', 'Rolls Royce', '10', NULL);
+-- (2,'Kindle',30000,14,4,'https://picsum.photos/id/367/2400/1600',''),
+-- (3,'Camera',15000,12,4.2,'https://picsum.photos/id/250/2400/1600',''),
+-- (4,'Clock',600,13,4.2,'https://picsum.photos/id/175/2400/1600',''),
+-- (5,'Joystick',3500,13,3.9,'https://picsum.photos/id/96/2400/1600',''),
+-- (6,'Rolls Royce',2500000,13,4.8,'https://picsum.photos/id/111/2400/1600','');
