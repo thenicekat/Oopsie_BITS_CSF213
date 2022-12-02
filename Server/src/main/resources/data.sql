b02c224e7b2e8634f13283616aaad53c9040c6a5
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-12-02 21:44:14 +0530

    Make modifications to data.sql

--- Server/src/main/resources/data.sql
+++ Server/src/main/resources/data.sql
@@ -1,14 +1,6 @@
-INSERT INTO users VALUES(123,'Atharva','Dashora','user1@xyz.com','u1@123',15000),
-(125,'Adarsh','Das','user2@xyz.com','user2@123',10000),(201,'Murari','Karthik','user3@xyz.com','u3@123',8900);
-
 INSERT INTO products VALUES(1,'MacBook',96900,12,4.6,'https://picsum.photos/id/180/2400/1600',''),
 (2,'Kindle',30000,,14,4,'https://picsum.photos/id/367/2400/1600',''),
 (3.'Camera',15000,12,4.2,'https://picsum.photos/id/250/2400/1600',''),
 (4,'Clock',600,13,4.2,'https://picsum.photos/id/175/2400/1600',''),
 (5,'Joystick',3500,13,3.9,'https://picsum.photos/id/96/2400/1600',''),
 (6,'Rolls Royce',2500000,13,4.8,'https://picsum.photos/id/111/2400/1600','');
-
-INSERT into admin VALUES(901,'true'),(203,'false'),(101,'false');
-
-INSERT into manager VALUES(1,'true','true','Divya'),(2,'true','false','-'),(3,'true','false','-'),
-(4,'true','false','-');
