48bcbd254c264105f613803173da74ae1ac9b001
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-12-06 15:28:45 +0530

    Temporary Push

--- Server/src/main/java/com/oopsie/shoppingapp/OopsieServer.java
+++ Server/src/main/java/com/oopsie/shoppingapp/OopsieServer.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OopsieServer {
 	public static void main(String[] args) {
 		try {
-			SendEmail.sendmail("divyateja2004@gmail.com", "App is successfully deployed");
+			SendEmail.sendmail("divyateja2004@gmail.com", "App was successfully deployed");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
