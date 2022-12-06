50c6bcd6da630bc67ddcb9190fec5fd35e4e6ce5
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-12-06 15:45:26 +0530

    Update SendEmail Class

--- Server/src/main/java/com/oopsie/shoppingapp/SendEmail.java
+++ Server/src/main/java/com/oopsie/shoppingapp/SendEmail.java
@@ -17,34 +17,35 @@ import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 
 public class SendEmail {
-    public static void sendmail(String email, String subject) throws AddressException, MessagingException, IOException {
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-        
-        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-           protected PasswordAuthentication getPasswordAuthentication() {
-              return new PasswordAuthentication("oopsieshoppingapp@gmail.com", "pdjnghxfxxxixtgm");
-           }
-        });
-        Message msg = new MimeMessage(session);
-        msg.setFrom(new InternetAddress("oopsieshoppingapp@gmail.com", false));
-     
-        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
-        msg.setSubject(subject);
-        msg.setContent("You are receiving this mail from OOPSIE", "text/html");
-        msg.setSentDate(new Date());
-     
-        MimeBodyPart messageBodyPart = new MimeBodyPart();
-        messageBodyPart.setContent("You are receiving this mail from OOPSIE", "text/html");
-     
-        Multipart multipart = new MimeMultipart();
-        multipart.addBodyPart(messageBodyPart);
-        msg.setContent(multipart);
-        Transport.send(msg);   
-     }
+   public static void sendmail(String email, String subject) throws AddressException, MessagingException, IOException {
+      Properties props = new Properties();
+      props.put("mail.smtp.auth", "true");
+      props.put("mail.smtp.starttls.enable", "true");
+      props.put("mail.smtp.host", "smtp.gmail.com");
+      props.put("mail.smtp.port", "587");
+      props.put("mail.smtp.starttls.required", "true");
+      props.put("mail.smtp.ssl.protocols", "TLSv1.2");
 
+      Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+         protected PasswordAuthentication getPasswordAuthentication() {
+            return new PasswordAuthentication("oopsieshoppingapp@gmail.com", "pdjnghxfxxxixtgm");
+         }
+      });
+      Message msg = new MimeMessage(session);
+      msg.setFrom(new InternetAddress("oopsieshoppingapp@gmail.com", false));
+
+      msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+      msg.setSubject(subject);
+      msg.setContent("You are receiving this mail from OOPSIE", "text/html");
+      msg.setSentDate(new Date());
+
+      MimeBodyPart messageBodyPart = new MimeBodyPart();
+      messageBodyPart.setContent("You are receiving this mail from OOPSIE", "text/html");
+
+      Multipart multipart = new MimeMultipart();
+      multipart.addBodyPart(messageBodyPart);
+      msg.setContent(multipart);
+      Transport.send(msg);
+   }
 
 }
