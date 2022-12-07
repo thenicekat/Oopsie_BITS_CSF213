ea73a0b3586357427e16de7b2079369f56d19539
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-12-08 00:30:21 +0530

    Website link change

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,1 @@
-export const SERVER_URL=process.env.SERVER_URL || "http://localhost:8080";
-// https://oopsie-production.up.railway.app"
+export const SERVER_URL="https://oopsie-production.up.railway.app/";
